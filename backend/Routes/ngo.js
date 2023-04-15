a762d0426557a176047703e4ad2881d3d0263592
Parth Barai <parthnbarai1234@gmail.com>
2023-04-15 23:13:53 +0530

    hospital and ngo login done

--- backend/Routes/ngo.js
+++ backend/Routes/ngo.js
@@ -36,4 +36,45 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+    try {
+        const userFind = await NgoSchema.findOne({ email: req.body.email })
+        if (userFind) {
+            const result = compareSync(req.body.password, userFind.password)
+            if (result) {
+
+                userFind.password = undefined;
+                const newUserToken = {
+                    name: userFind.name,
+                    email: userFind.email
+                }
+                const jsontoken = await auth.tokenGenerate(newUserToken, req, res);
+                return res.status(200).json({
+                    success: 1,
+                    message: "Successful signup",
+                    token: jsontoken,
+                });
+            }
+            else {
+                // console.log(err.message)
+                return res.status(403).json({
+                    error: {
+                        message: "Username or Password Invalid!"
+                    }
+                })
+            }
+        }
+        else {
+            return res.status(403).json({
+                error: {
+                    message: "Username or Password Invalid!"
+                }
+            })
+        }
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json(err.message);
+    }
+})
+
 module.exports = router
