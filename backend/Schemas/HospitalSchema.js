ae4f2ecffe44d545ddf644cea110ad9398c0ebfd
Chris <chrisdias2311@gmail.com>
2024-10-10 23:33:09 +0530

    Location and SOS bugs solved

--- backend/Schemas/HospitalSchema.js
+++ backend/Schemas/HospitalSchema.js
@@ -1,5 +1,48 @@
+// const mongoose = require("mongoose");
+// // console.log(user-icon)
+// const hospitalSchema = mongoose.Schema({
+//     email: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     phone: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     address: {
+//         type: String,
+//         required: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     },
+//     regNo: {
+//         type: String,
+//         required: true
+//     },
+//     latitude: {
+//         type: String,
+//         required: true
+//     },
+//     longitude: {
+//         type: String,
+//         required: true
+//     }
+// });
+
+
+// module.exports = mongoose.model('Hospital', hospitalSchema);
+
+
 const mongoose = require("mongoose");
-// console.log(user-icon)
+
 const hospitalSchema = mongoose.Schema({
     email: {
         type: String,
@@ -26,9 +69,14 @@ const hospitalSchema = mongoose.Schema({
     regNo: {
         type: String,
         required: true
+    },
+    location: {
+        type: { type: String, enum: ['Point'], required: true },
+        coordinates: { type: [Number], required: true }
     }
-
 });
 
+// Create a geospatial index on the location field
+hospitalSchema.index({ location: '2dsphere' });
 
 module.exports = mongoose.model('Hospital', hospitalSchema);
